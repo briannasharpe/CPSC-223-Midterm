f47a7856f5a4550f03423c085fa454e98018c858
briannasharpe <36048465+briannasharpe@users.noreply.github.com>
2019-03-16 23:38:02 -0700

    Add files via upload

--- problem 2.py
+++ problem 2.py
@@ -6,3 +6,10 @@ Created on Sat Mar 16 22:39:27 2019
 @author: heroname
 """
 
+class student_list(list):
+    
+    
+    
+    
+num_students = int(input("Number of students: "))
+student_info = input("Student data: ")
