3a113b928e116047f1f2ca68cd708f4b0636ac08
briannasharpe <36048465+briannasharpe@users.noreply.github.com>
2019-03-18 10:48:22 -0700

    Add files via upload

--- problem_2.py
+++ problem_2.py
@@ -5,18 +5,19 @@ Created on Sat Mar 16 22:39:27 2019
 
 @author: heroname
 """
-from problem_1 import Student
+from problem_1 import student
 
 class student_list(list):
     pass # placeholder, no executed code
     
 def main():
     student_data = student_list()
-    the_list = ()
     
     num_students = int(input("Number of students: "))
     
-    for a in num_students:
+    for a in range(int(num_students)):
+        temp_list = []
+        
         data = input("Student data: ")
         
         # split data into sections
@@ -25,10 +26,16 @@ def main():
         for e in data_list:
             # separate key and value from data sections
             key, value = e.split(':')
-            
-            the_list.append(value)
+            # append to temp_list
+            temp_list.append(value)
+        # ---------- end for loop (data_list) ----------
+        # create student
+        a_student = student(temp_list)
+        # append student to student database
+        student_data.append(a_student)
+    # ---------- end for loop (range(int(num_students))) ----------
     
+    print(student_data)
+    # ----- end main() -----
 
-    # ---------- end for loop (data_list) ----------
-
-print(student_list(student_info))
+main()
---
+++ problem_3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 23:21:15 2019
+
+@author: heroname
+"""
+
