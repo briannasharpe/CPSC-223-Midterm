c94c1e35497d5d4d02e35397e385e91407f84f08
briannasharpe <36048465+briannasharpe@users.noreply.github.com>
2019-03-17 19:16:03 -0700

    Add files via upload

--- problem_2.py
+++ problem_2.py
@@ -5,28 +5,30 @@ Created on Sat Mar 16 22:39:27 2019
 
 @author: heroname
 """
-from problem_1 import student
+from problem_1 import Student
 
 class student_list(list):
-    def __init__(self, data):
-        self._data = data
+    pass # placeholder, no executed code
     
-        # define variables
-        a_list = []
+def main():
+    student_data = student_list()
+    the_list = ()
     
+    num_students = int(input("Number of students: "))
+    
+    for a in num_students:
+        data = input("Student data: ")
+        
         # split data into sections
         data_list = data.split(', ')
         
         for e in data_list:
             # separate key and value from data sections
             key, value = e.split(':')
-            # add key and value to student dictionary
-            a_list.append({key:value})
-        # ---------- end for loop (data_list) ----------
-        
-        return a_list
+            
+            the_list.append(value)
     
-num_students = int(input("Number of students: "))
-student_info = input("Student data: ")
+
+    # ---------- end for loop (data_list) ----------
 
 print(student_list(student_info))
